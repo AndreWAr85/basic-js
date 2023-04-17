6ebbb4cc9274fa2023f0d762490ecef8182c2161
AndreWAr85 <andreyarabchyk@gmail.com>
2023-04-17 21:38:21 +0200

    feat; add what-seasons

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date) {
+   let time = date.getTime();
+   console.log(time)
 
+   let Y = date.getFullYear();
+   let monthIndex = date.getMonth();
+   let M = monthIndex + 1;
+   let D = date.getDate();
+   console.log(Y)
+   console.log(M)
+   console.log(D)
+   if (time !== undefined || null) {
+      if (isValidDate(Y, M, D) === false) {
+         return 'Error'
+      }
+   } else { return 'Unable to determine the time of year!' }
+
+   function isValidDate(Y, M, D) {
+      var d = new Date(Y, --M, D);
+      return Y == d.getFullYear() && M == d.getMonth() && D == d.getDate();
+   };
+
+   let utc = date.toUTCString();
+   console.log(utc)
+   let String = date.toDateString();
+   console.log(String)
+
+   let d = date.getDay();
+   console.log(d + " dni")
+   let H = date.getHours()
+   console.log(H + " godzin")
+   let minutes = date.getMinutes();
+   console.log(minutes + " minut")
+   let seconds = date.getSeconds();
+   console.log(seconds + " sekund")
+
+   if (M < 3 || M == 12) {
+      return ("winter")
+   } else if (M < 6) {
+      return ("spring")
+   } else if (M < 9) {
+      return ("summer")
+   } else { return ("autumn (fall)") }
 }
 
+getSeason(new Date(2020, 02, 31))
 module.exports = {
    getSeason
 };
